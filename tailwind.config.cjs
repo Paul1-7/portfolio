d1fec632525d8aad1ea2bc27e74fe6cd9b02a263
Paul <moicoxoxo@gmail.com>
2023-01-01 14:24:07 -0400

    add meta tags

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+// const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   darkMode: 'class',
   content: ['./src/js/*.js', './index.html'],
@@ -26,8 +28,6 @@ module.exports = {
         'btn-hover':
           'linear-gradient(60deg, #f79533, #f37055, #ef4e7b, #a166ab, #5073b8, #1098ad, #07b39b, #6fba82);',
         'text-color': 'linear-gradient(45deg,#d42b56,#ef9070,#b3287e);'
-        // 'background-light': 'url(./assets/svg/background-light.svg)',
-        // 'background-dark': 'url(./assets/svg/background-dark.svg)'
       },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif']
